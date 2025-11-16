f9521cfeadc9aa84b3323e257b512770e5c06bcd
Hifzan <hifzan.ahmed@gmail.com>
2025-11-16 23:21:45 +0100

    Test4

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import os
 
 def main():
     #location_of_training_signature = 'C:/Users/hifza/workspace/Signature Dataset/signatures_6/original_6_'
-    location_of_training_signature = 'C:/Users/hifza/workspace/Signature Dataset/Saba/signature'
+    location_of_training_signature = 'C:/Users/hifza/workspace/Signature Dataset/Neha/signature'
     size_of_training_signature = 6
     #location_of_test_signature = 'C:/Users/hifza/workspace/Signature Dataset/signatures_6/original_6_'
-    location_of_test_signature = 'C:/Users/hifza/workspace/Signature Dataset/Saba/Forged/signature'
+    location_of_test_signature = 'C:/Users/hifza/workspace/Signature Dataset/Neha/signature'
     # Training Phase
     s1 = SignatureTraining.training_genuine_with_shape_dtw(location_of_training_signature, size_of_training_signature)  
     # Verification Phase of input test signature 
